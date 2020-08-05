a41e0fda2c75c17293b20c899c0ee941834c8514
itouma <68621609+itouma@users.noreply.github.com>
2020-08-06 08:47:32 +0900

    JSON形式で返す手順

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to:'posts#new'
+  root to: 'posts#index'
+  # get 'posts/new', to:'posts#new'
   post 'posts',to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
 end
 
-<%= form_with url: "/posts", method: :post, local: true do |form| %>
-  <%= form.text_field :content %>
-  <%= form.submit '投稿する' %>
-<% end %>
+# <%= form_with url: "/posts", method: :post, local: true do |form| %>
+#   <%= form.text_field :content %>
+#   <%= form.submit '投稿する' %>
+# <% end %>
